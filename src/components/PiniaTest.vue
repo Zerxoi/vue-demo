88d544c78859cc992cccae422946049cef95c013
zerxoi <zerxoi1997@gmail.com>
2022-06-26 11:56:23 +0800

    Pinia getters&actions

--- src/components/PiniaTest.vue
+++ src/components/PiniaTest.vue
@@ -6,8 +6,10 @@ const main = useMainStore()
 
 <template>
     <div>
+        <button @click="main.getLoginInfo">Login</button>
         <div>{{ main.counter }}</div>
         <div>{{ main.name }}</div>
         <div>{{ main.doubleCount }}</div>
+        <div>{{ main.doubleCountPlusOne }}</div>
     </div>
 </template>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,25 +1,40 @@
 import { defineStore } from "pinia";
 
-export const useMainStore = defineStore('main', {
-    // a function that returns a fresh state
-    state: () => ({
-      counter: 99,
-      name: 'Eduardo',
-    }),
-    // optional getters
-    getters: {
-      // getters receive the state as first parameter
-      doubleCount: (state) => state.counter * 2,
-      // use getters in other getters
-      doubleCountPlusOne(): number {
-        return this.doubleCount + 1
-      },
-    },
-    // optional actions
-    actions: {
-      reset() {
-        // `this` is the store instance
-        this.counter = 0
-      },
-    },
+const login = (): Promise<{ username: string }> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        username: 'admin'
+      })
+    }, 3000)
   })
+}
+
+export const useMainStore = defineStore('main', {
+  // a function that returns a fresh state
+  state: () => ({
+    counter: 99,
+    name: 'Eduardo',
+  }),
+  // optional getters
+  getters: {
+    // getters receive the state as first parameter
+    // doubleCount: (state) => state.counter * 2,
+    doubleCount(): number {
+      return this.counter * 2;
+    },
+    // use getters in other getters
+    doubleCountPlusOne(): number {
+      return this.doubleCount + 1
+    },
+  },
+  actions: {
+    increment() {
+      this.counter++
+    },
+    async getLoginInfo() {
+      let loginInfo = await login()
+      console.log(loginInfo.username)
+    }
+  },
+})
